1187c973222852fd44b1249280cc861fa3cd2f3c
Andrei <a.parchukov@yandex.ru>
2022-08-07 02:47:50 +0300

    init

--- SuffixSequence/ContentView.swift
+++ SuffixSequence/ContentView.swift
@@ -7,6 +7,44 @@
 
 import SwiftUI
 
+struct SuffixSequence: Sequence {
+    
+    let letter: String
+    
+    func makeIterator() -> SuffixIterator {
+        SuffixIterator(self)
+    }
+    
+}
+
+struct SuffixIterator: IteratorProtocol {
+    
+    let suffixSequence: SuffixSequence
+    var startIndex: String.Index
+    
+    init(_ sequence: SuffixSequence) {
+        suffixSequence = sequence
+        startIndex = sequence.letter.startIndex
+    }
+    
+    mutating func next() -> String? {
+        let letter = suffixSequence.letter
+        guard !letter.isEmpty else { return nil }
+        
+        let suffix = letter.suffix(from: startIndex)
+        if !suffix.isEmpty {
+            startIndex = letter.index(after: startIndex)
+            return String(suffix)
+        } else {
+            return nil
+        }
+    }
+    
+}
+
+let sequence = SuffixSequence(letter: "word")
+
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")

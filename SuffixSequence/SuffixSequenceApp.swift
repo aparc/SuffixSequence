952cb4470018f13a0a0f8ba9f6f6a2c37f20579a
Andrei <a.parchukov@yandex.ru>
2022-08-06 20:59:01 +0300

    Initial Commit

---
+++ SuffixSequence/SuffixSequenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuffixSequenceApp.swift
+//  SuffixSequence
+//
+//  Created by Андрей Парчуков on 06.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SuffixSequenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
